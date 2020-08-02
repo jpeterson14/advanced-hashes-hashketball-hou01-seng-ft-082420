e74971ad841db761f2ca9e9530f505b32e67c718
Jake Peterson <jpeterson0514@gmail.com>
2020-08-02 01:28:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,10 +192,11 @@ end
   
   
   def big_shoe_rebounds
-    biggest shoe = 0 
+    biggest_shoe = 0 
     rebounds = 0 
     game_hash.each do |location, team_data|
-      team_data[:players].each do |player| 
+      team_data[:players].each do |player|
+        if player[:shoe] > biggest_shoe
   end 
   
 
